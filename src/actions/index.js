d28aca23327d30d62d046b7e7e890eabebbf6b5d
camimaya21 <camilamaya21@gmail.com>
2018-03-28 13:33:30 +0200

    added the action creator to make a call to the blog api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,14 @@
+import axios from 'axios';
+import { API_KEY } from 'babel-dotenv';
+
 export const FETCH_POSTS = 'fetch_posts';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 
 export function fetchPost() {
+  const request = axios.get(`${ROOT_URL}/posts/?key=${API_KEY}`);
+
   return {
-    type: FETCH_POSTS
+    type: FETCH_POSTS,
+    payload: request
   };
 }
